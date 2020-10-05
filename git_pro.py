80c5f33e691f8cc67477347519caa0c9a70c48da
aleksisiirtola <aleksijuliussiirtola@gmail.com>
2020-10-05 12:05:48 +0300

    adds new class.

--- git_pro.py
+++ git_pro.py
@@ -1,2 +1,23 @@
 def hello():
     return "Hello World! 22"
+
+class person():
+
+    def __init__(self):
+        self.name = "Joni"
+        self.age = "22"
+
+    def namer(self, set_name):
+        self.name = str(set_name)
+
+    def ager(self, set_age):
+        self.age = str(set_age)
+
+    def my_name(self):
+        return(self.name)
+
+    def my_age(self):
+        return(self.age)
+
+    def __str__:
+        return "Testing"
