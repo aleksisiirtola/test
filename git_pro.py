f9341ef241bad286dbaf7ab1eb2d1e39b1350e2d
aleksisiirtola <aleksijuliussiirtola@gmail.com>
2020-10-05 12:23:09 +0300

    overwrites Person with file from https://github.com/academygit/MoreBranchingAndMerging/blob/master/Person.py

--- git_pro.py
+++ git_pro.py
@@ -1,23 +1,32 @@
-def hello():
-    return "Hello World! 224"
+class Person:
+    def __init__(self, city, address, phone_number):
+        self._city = city
+        self._address = address
+        self._phone_number = phone_number
 
-class person():
+    @property
+    def city(self, _default = None):
+        return self._city
 
-    def __init__(self):
-        self.name = "Joni"
-        self.age = "22"
+    @city.setter
+    def city(self, value):
+        self._city = value
 
-    def namer(self, set_name):
-        self.name = str(set_name)
+    @property
+    def address(self):
+        return self._address
 
-    def ager(self, set_age):
-        self.age = str(set_age)
+    @address.setter
+    def address(self, value):
+        self._address = value
 
-    def my_name(self):
-        return(self.name)
+    @property
+    def phone_number(self, _default=None):
+        return self._phone_number
 
-    def my_age(self):
-        return(self.age)
+    @phone_number.setter
+    def phone_number(self, value):
+        self._phone_number = value
 
-    def __str__:
-        return "Testing"
+    def __str__(self):
+        return f'Address: {self._address}\nCity: {self._city}\nPhone: {self._phone_number}'
